c6d3a7c3a22375ac11cc044c2570c5ab788a277b
RicchiutiLucia <luciaricchiuti04@gmail.com>
2023-02-28 16:11:46 +0100

    completamento esercizio base

--- js/main.js
+++ js/main.js
@@ -7,34 +7,39 @@ const imagesArray = [
 ];
 
 const imageListDom = document.querySelector('.img-list');
+
 let imageContent = "";
+
 for(let i = 0; i < imagesArray.length; i++){
     const newImageWrapper = `<div class="img-wrapper">
                                 <img class="image" src="${imagesArray[i]}">
                               </div>`;
 
-    
     imageContent += newImageWrapper;
     
 }
 
 imageListDom.innerHTML = imageContent;
 
+
 const imagesWrapperDom = document.getElementsByClassName('img-wrapper');
 //console.log(imageWrapperDom);
 
 let activeImage = 0;
+
 imagesWrapperDom[activeImage].classList.add('show');
+
 
 const nextDom = document.querySelector('#next');
 const prevDom = document.querySelector('#prev');
 
 nextDom.addEventListener('click', 
     function(){
-        if(activeImage < imagesWrapperDom.length -1){
+        if(activeImage < imagesWrapperDom.length - 1){
             imagesWrapperDom[activeImage].classList.remove('show');
             activeImage++;
             imagesWrapperDom[activeImage].classList.add('show');
+            
         }
         
     }
@@ -46,7 +51,6 @@ prevDom.addEventListener('click',
             imagesWrapperDom[activeImage].classList.remove('show');
             activeImage--;
             imagesWrapperDom[activeImage].classList.add('show');
-
         }
         
     }
